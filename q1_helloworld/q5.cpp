ec203280b23784fd131bb221f73487b59a340ca0
nishthanarula89 <Nnarula_be24@thapar.edu>
2026-01-20 00:10:13 +0530

    Create q5.cpp

---
+++ q1_helloworld/q5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "oops\nAssignment" << endl;
+    cout << "oops\tAssignment" << endl;
+    cout << "oops\bAssignment" << endl;
+
+    cout << setw(15) << "oops" << endl;
+    
+}
