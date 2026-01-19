a9be13ae76d078aae32278393b28e99501adb900
nishthanarula89 <Nnarula_be24@thapar.edu>
2026-01-20 00:01:36 +0530

    Create q4.cpp

---
+++ q1_helloworld/q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> a;
+
+    b = (9*a)/5 + 32;
+
+    cout << "Temperature in Fahrenheit =  " <<b<<endl;
+  
+}
